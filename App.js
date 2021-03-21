eb7ca67b8ce48c7032b868a11fc014b39e1dc2db
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-03-20 22:22:50 -0300

    iniciado

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import params from './src/params';
+
+
+export default class App extends Component {
+  render(){
+    return (
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Iniciando o Mines</Text>
+        <Text style={styles.sectionDescription}>Tamanho de grade:
+        {params.getColumnsAmount()}x{params.getColumnsAmount()}
+        </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    backgroundColor:'#FFFFFF',
+    flex: 1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
