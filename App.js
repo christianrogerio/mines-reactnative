6a2f1c4b0799aa4246095094e3f2e7eab17ddff3
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-03-20 22:55:40 -0300

    layout das casas das minas feito

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   View,
 } from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field'
 
 
 export default class App extends Component {
@@ -20,6 +21,16 @@ export default class App extends Component {
         <Text style={styles.sectionDescription}>Tamanho de grade:
         {params.getColumnsAmount()}x{params.getColumnsAmount()}
         </Text>
+        <Field/>
+        <Field opened/>
+        <Field opened nearMines={1}/>
+        <Field opened nearMines={2}/>
+        <Field opened nearMines={3}/>
+        <Field opened nearMines={4}/>
+        <Field opened nearMines={5}/>
+        <Field opened nearMines={6}/>
+        <Field opened nearMines={7}/>
+        <Field opened nearMines={8}/>
       </View>
     )
   }
