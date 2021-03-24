f3903e68c4786ee156b74010ffde6a315fce7946
Christian Câmara de Abreu <christian@MacBook-Air-de-Christian.local>
2021-03-24 19:52:05 -0300

    desenvolvidas funcoes para criar tabuleiro e espalhar minas

---
+++ src/components/functions.js
@@ -0,0 +1,36 @@
+const creatBoard = (rows, cloumns)=>{
+  return Array(rows).fill(0).map( (_,row)=>{
+    return Array(cloumns).fill(0).map( (_column)=>{
+      return{
+        row,column,
+        opened:false,
+        flagged:false,
+        mined:false,
+        exploded:false,
+        nearMines:0,
+      }
+    })
+  })
+}
+
+const spreadMines=(board,minesAmount)=>{
+  const rows=board.length
+  const columns=board[0].length
+  let minesPlanted=0
+  while(minesPlanted < minesPlanted){
+    const rowSel=parseInt(Math.random()*rows,10)
+    const columnSel=parseInt(Math.random()*columns,10)
+    if(!board[rowSel][columnSel].mined){
+      board[rowSel][columnSel].mined=true
+      minesPlanted++
+    }
+  }
+}
+
+const createMineBoard=(rows,columns,minesAmount)=>{
+  const board=creatBoard(rows,columns)
+  spreadMines(board,minesAmount)
+  return board
+}
+
+export {createMineBoard}
